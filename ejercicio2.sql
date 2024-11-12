1057f5dd6f73a47d87564a08c2094ba4af382e2e
Mateo Matus <110347934+SkillFP@users.noreply.github.com>
2024-11-12 16:38:46 -0300

    2

--- ejercicio2.sql
+++ ejercicio2.sql
@@ -35,3 +35,45 @@ FROM Arbol a
 INNER JOIN Localidad l ON a.codigoPostal=l.codigoPostal
 LEFT JOIN Poda p ON a.nroArbol=p.nroArbol
 WHERE p.codPoda IS NULL;
+
+/*4. Reportar especie, años,calle, nro y localidad de árboles que fueron podados durante 2022 y no
+fueron podados durante 2023.*/
+
+SELECT DISTINCT especie, años, calle , nro, nombreL
+FROM Arbol a
+INNER JOIN Localidad l ON a.codigoPostal=l.codigoPostal
+INNER JOIN Poda p ON p.nroArbol=a.nroArbol
+WHERE p.fecha BETWEEN '2022-01-01' AND '2022-12-31'
+EXCEPT (
+    SELECT DISTINCT especie, años, calle , nro, nombreL
+    FROM Arbol a
+    INNER JOIN Localidad l ON a.codigoPostal=l.codigoPostal
+    INNER JOIN Poda p ON p.nroArbol=a.nroArbol
+    WHERE p.fecha BETWEEN '2023-01-01' AND '2023-12-31'
+);
+
+/*5. Reportar DNI, nombre, apellido, fecha de nacimiento y localidad donde viven de aquellos
+podadores con apellido terminado con el string ‘ata’ y que tengan al menos una poda durante
+2024. Ordenar por apellido y nombre*/
+
+SELECT DISTINCT pp.DNI, pp.nombre, pp.apellido, pp.fnac, l.nombreL
+FROM Podador pp
+INNER JOIN Localidad l ON pp.codigoPostalVive=l.codigoPostal
+INNER JOIN Poda p ON pp.DNI=p.DNI
+WHERE pp.apellido LIKE '%ata' AND p.fecha BETWEEN '2024-01-01' AND '2024-12-31'
+ORDER BY pp.apellido, pp.nombre;
+
+/*6. Listar DNI, apellido, nombre, teléfono y fecha de nacimiento de podadores que solo podaron
+árboles de especie ‘Coníferas’.*/
+
+SELECT pp.DNI, pp.apellido, pp.nombre, pp.telefono, pp.fnac
+FROM Podador pp
+INNER JOIN Poda p ON p.DNI=pp.DNI
+INNER JOIN Arbol a ON p.nroArbol=a.nroArbol
+WHERE a.especie = 'Coníferas' AND NOT EXISTS (
+    SELECT 1
+    FROM Podador pp2
+    INNER JOIN Poda p2 ON p2.DNI = pp2.DNI
+    INNER JOIN Arbol a2 ON a2.nroArbol=p2.nroArbol
+    WHERE a.especie != 'Coníferas'
+);
